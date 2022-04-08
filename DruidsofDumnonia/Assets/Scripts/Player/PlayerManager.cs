db7e4ebfa139527ad97e46875aa1b8d8b73450eb
Michael <55700937+mriley-wallace@users.noreply.github.com>
2022-04-08 21:35:57 +0100

    AI Scripting 1 - NavMesh added, agent scripting added for follow and lookAt player

---
+++ DruidsofDumnonia/Assets/Scripts/Player/PlayerManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    public static PlayerManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject player;
+
+
+}
