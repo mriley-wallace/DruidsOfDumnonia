d94cb116177c55f5529ecddf99a08af6dcd83362
Michael <55700937+mriley-wallace@users.noreply.github.com>
2022-05-11 22:47:22 +0100

    $123 Update to buttons to include previous booleans to be checked too

--- DruidsofDumnonia/Assets/Scripts/Player/ProgressButton.cs
+++ DruidsofDumnonia/Assets/Scripts/Player/ProgressButton.cs
@@ -17,22 +17,24 @@ public class ProgressButton : MonoBehaviour
             GameObject.FindGameObjectWithTag("Player").transform.position = AirStart.transform.position;
         }
 
-        if(enemy.AirDead == true)
+        if(enemy.AirDead == true && enemy.WaterDead == true)
         {
             GameObject.FindGameObjectWithTag("Player").transform.position = ThunderStart.transform.position;
         }
 
-        if (enemy.ThunderDead == true)
+        if (enemy.ThunderDead == true && enemy.AirDead == true && enemy.WaterDead == true)
         {
             GameObject.FindGameObjectWithTag("Player").transform.position = FireStart.transform.position;
         }
 
-        if (enemy.FireDead == true)
+        if (enemy.FireDead == true && enemy.ThunderDead == true && enemy.AirDead == true && enemy.WaterDead == true)
         {
             GameObject.FindGameObjectWithTag("Player").transform.position = FightStart.transform.position;
         }
 
+        this.gameObject.SetActive(false);
     }
+
 
 
 
